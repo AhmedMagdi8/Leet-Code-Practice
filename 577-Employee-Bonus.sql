7a13ffbb658f6661ebcf0e140d97db1cbe820e74
Ahmed Magdi <amagdi825@gmail.com>
2024-09-07 09:09:31 +0300

    [577] [Time Beats: 5.80%] - LeetPush

---
+++ 577-Employee-Bonus.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+
+select Employee.name, Bonus.bonus from Employee left join Bonus on Employee.empId = Bonus.empId where Bonus.bonus < 1000 or Bonus.bonus IS NULL;
